a54617fc1ec50121e97f193343f037aad5096dfe
LottoTang <tanglon@oregonstate.edu>
2023-02-08 00:43:40 +0800

    trial5

--- backend/exercise-controller.mjs
+++ backend/exercise-controller.mjs
@@ -22,22 +22,23 @@ import validateRequestSchema from './middleware/validate-request-schema.mjs';
 import validSchema from './middleware/validate-schema.mjs';
 
 const PORT = process.env.PORT;
+
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'https://cs290-workout.onrender.com', 'https://cs290-workout.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
+
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000'];
-
-const options = cors.CorsOptions = {
-  origin: allowedOrigins
-};
-
-app.use(cors(options));
-
 app.use(express.json());
-app.use(
-    cors({
-        origin: ["http://localhost:3000", "https://cs290-workout-api.onrender.com"],
-    })
-);
 
 // CREATE controller ******************************************
 // create middleware to check the incoming data is valid or not
